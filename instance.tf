65f8dcd2bcf244760b2c811f7b45247f19a00cc2
Mahtab Hossen Jhalok <48786676+Mahtabhj@users.noreply.github.com>
2023-07-14 08:40:24 +0600

    Update instance.tf

--- instance.tf
+++ instance.tf
@@ -10,8 +10,8 @@ resource "aws_vpc" "main" {
 }
 
 resource "aws_subnet" "subnet1" {
-  vpc_id     = aws_vpc.main.id
-  cidr_block = "10.0.1.0/24"
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.1.0/24"
   availability_zone = "us-east-1a"
 
   tags = {
@@ -20,15 +20,14 @@ resource "aws_subnet" "subnet1" {
 }
 
 resource "aws_subnet" "subnet2" {
-  vpc_id     = aws_vpc.main.id
-  cidr_block = "10.0.2.0/24"
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.2.0/24"
   availability_zone = "us-east-1b"
 
   tags = {
     Name = "elb-subnet-2"
   }
 }
-
 
 resource "aws_internet_gateway" "main-igw" {
   vpc_id = aws_vpc.main.id
@@ -38,23 +37,12 @@ resource "aws_internet_gateway" "main-igw" {
   }
 }
 
-resource "aws_eip" "nat" {}
-
-resource "aws_nat_gateway" "main-natgw" {
-  allocation_id = aws_eip.nat.id
-  subnet_id     = aws_subnet.subnet2.id
-
-  tags = {
-    Name = "main-nat"
-  }
-}
-
 resource "aws_route_table" "main-public-rt" {
   vpc_id = aws_vpc.main.id
 
   route {
     cidr_block     = "0.0.0.0/0"
-    nat_gateway_id = aws_nat_gateway.main-natgw.id
+    gateway_id     = aws_internet_gateway.main-igw.id
   }
 
   tags = {
@@ -73,8 +61,8 @@ resource "aws_route_table_association" "public-assoc-2" {
 }
 
 resource "aws_security_group" "bastion-sg" {
-  name   = "bastion-security-group"
-  vpc_id = aws_vpc.main.id
+  name        = "bastion-security-group"
+  vpc_id      = aws_vpc.main.id
 
   ingress {
     from_port   = 0
@@ -113,12 +101,11 @@ resource "aws_security_group" "elb-sg" {
 
 resource "aws_launch_configuration" "web_lc" {
   name                      = "web-launch-config"
-  image_id                  = "ami-01428ed083fc6ed14"
+  image_id                  = "ami-01428ed083fc6ed14"  # Replace with the actual AMI ID of "TestyImage"
   instance_type             = "t2.micro"
-  associate_public_ip_address = true  # Associate a public IP with instances launched from this launch configuration
+  associate_public_ip_address = true
   security_groups           = [aws_security_group.bastion-sg.id]
-  iam_instance_profile      = "arn:aws:iam::086508667280:instance-profile/AWSCodeDeployForEC2" 
-
+  iam_instance_profile      = "arn:aws:iam::086508667280:instance-profile/AWSCodeDeployForEC2"  # Replace with the instance profile ARN
 }
 
 resource "aws_autoscaling_group" "web_asg" {
@@ -137,10 +124,6 @@ resource "aws_autoscaling_group" "web_asg" {
     value               = "HelloWorldASG"
     propagate_at_launch = true
   }
-}
-
-data "aws_autoscaling_group" "web_asg_data" {
-  name = aws_autoscaling_group.web_asg.name
 }
 
 resource "aws_elb" "web_elb" {
